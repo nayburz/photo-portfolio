0736a02cc57da3a9b0e5c37634274b9a17b90ab9
Adrian Neighbors <adrian102@gmail.com>
2022-12-03 20:12:20 -0800

    Test commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="text-red-500">
         <p>Placeholder for main</p>
       </main>
 
